7d9600269335847796fbe8f412e920b9940a5c7f
isa kulaksız <isakulaksiz@outlook.com>
2022-01-30 11:08:38 +0300

    first commit

---
+++ app/src/main/java/com/example/fragment/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.fragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
